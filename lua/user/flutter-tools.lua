d5075ab8c888002ebdf623d4d95548933c8f8cda
= <=>
2024-03-13 11:00:54 +0700

    feat: add flutter-tools plugin

---
+++ lua/user/flutter-tools.lua
@@ -0,0 +1,16 @@
+
+local M = {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+}
+
+function M.config()
+  require("flutter-tools").setup()
+end
+
+return M
