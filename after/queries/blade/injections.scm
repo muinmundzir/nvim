7f0552cd80b60b464902aeecf1ef215b22c42445
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-06 07:47:52 +0700

    feat: add injections arguments

--- after/queries/blade/injections.scm
+++ after/queries/blade/injections.scm
@@ -14,3 +14,17 @@
     (#set! injection.language php_only))
 ((parameter) @injection.content
     (#set! injection.language php_only))
+
+(scoped_call_expression
+ scope: (name) @scope (#eq? @scope "DB")
+ name: (name) @name (#any-of? @name "insert" "select")
+ (arguments
+  (argument
+   (string
+    ((string_value) @injection.content
+     (#set! injection.language sql)
+    )
+   )
+  )
+ )
+)
