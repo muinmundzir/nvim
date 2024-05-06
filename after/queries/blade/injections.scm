7b74375db72388d632ba538640ce5b1c1d08778e
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-06 08:25:14 +0700

    refactor: remove unused injection argument

--- after/queries/blade/injections.scm
+++ after/queries/blade/injections.scm
@@ -14,17 +14,3 @@
     (#set! injection.language php_only))
 ((parameter) @injection.content
     (#set! injection.language php_only))
-
-(scoped_call_expression
- scope: (name) @scope (#eq? @scope "DB")
- name: (name) @name (#any-of? @name "insert" "select")
- (arguments
-  (argument
-   (string
-    ((string_value) @injection.content
-     (#set! injection.language sql)
-    )
-   )
-  )
- )
-)
